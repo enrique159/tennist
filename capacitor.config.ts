c31c584601f95ef5b9a3a4d4a86a920c3decac09
Enrique Marin <enrique_marin_h@hotmail.com>
2024-10-17 10:25:42 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tennist',
+  webDir: 'dist'
+};
+
+export default config;
